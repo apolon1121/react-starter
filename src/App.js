b0734fcdb9e86dfb525603fdc5004a9bf949901e
Apolon <a.pachulia@hotmail.com>
2018-03-04 12:18:38 +0400

    props and children

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
         <h1>Hi, I`m a React App</h1>
         <p>This is really working!</p>
         <Person name="Apolon" age="28" />
+        <Person name="Nika" age="27">My Hobbies</Person>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 
 const person = (props) => {
-    return <p>I am {props.name} and I am {props.age} years old</p>
+    return (
+        <div>
+            <p>I am {props.name} and I am {props.age} years old</p>
+            <p>{props.children}</p>
+        </div>    
+    )
 }
 
 export default person;
